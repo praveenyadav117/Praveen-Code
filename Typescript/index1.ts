dc280f7e636e298aa9a88e847d876025c317d4c8
praveen kumar <praveenkumary11799@gmail.com>
2025-12-12 08:12:05 +0530

    types annotations and type inference

---
+++ Typescript/index1.ts
@@ -0,0 +1,7 @@
+// type annotations --> have to tell the types of varible expecility
+// type infrence --> typescript automatically take the type without defining it 
+
+let myName = "Praveen";
+// myName = 2;
+let surName : string = "Yadav";
+surName = "Kumar";

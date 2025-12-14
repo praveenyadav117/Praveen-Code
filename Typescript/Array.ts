dda237db7a06b03e30d9f151f574d368f343f2e2
praveen kumar <praveenkumary11799@gmail.com>
2025-12-14 08:31:59 +0530

    Arrays tuples

---
+++ Typescript/Array.ts
@@ -0,0 +1,30 @@
+const chaiTyep : string[] = ['masala','adarak'];
+const chaiProice : Array<number> = [10,20];//number[]
+
+type chaiObject = {
+    name:string,
+    price : number[]
+}
+
+const menu : chaiObject [] = [{name:'masaal',price :[10,20]}];
+console.log("menu",menu);
+
+menu.push({name:'adrak',price:[20,30]});
+console.log("menu===11",menu);
+
+const res = menu.filter(chai=> chai.name === 'masaal');
+console.log("menu222=====",menu, res);
+
+const res1 = menu.map(chai=>{
+    if(chai.price.includes(10)){
+    menu.push({name:'new chai', price:[10,20,30]})
+    }
+})
+
+console.log("menu33333=====",menu, res1);
+
+
+
+const cities : readonly string[] = ['delhi','pune'];
+
+
